973563d8d0a10816291ea8e91f4275fe504e0cf8
Francisk <franciskyser77@gmail.com>
2025-11-26 12:28:45 -0500

    Update print message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def index():
-    return "Hello World"
+    return "Hello World!"
 
 if __name__ == "__main__":
    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
